290fb4eedd56a0994eeb4fd40ab0b8590424e4d7
mikemanuu <mikemanuu273@gmail.com>
2023-10-19 18:54:02 +0300

    addition

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, request, render_template, send_from_directory
 import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 from decouple import Config, Csv
 
 config = Config('.env')
+#config.read('.env')
 
 app = Flask(__name__, static_folder='static')
 
@@ -13,29 +16,42 @@ def index():
 # Load email credentials from environment variables
 email_username = config('EMAIL_USERNAME', default='username')
 email_password = config('EMAIL_PASSWORD', default='password')
+recipient_email = config('RECIPIENT_EMAIL', default='recipient')
 
-@app.route('/contact-form', methods=['POST'])
+@app.route('/send_email', methods=['POST'])
 def send_email():
     if request.method == 'POST':
-        name = request.form['name']
-        email = request.form['email']
-        subject = request.form['subject']
-        message = request.form['message']
-
-        smtp_server = 'smtp.gmail.com'
-        smtp_port = 587
-
-        msg = f"From: {email}\nSubject: {subject}\nName: {name}\nMessage:\n{message}"
-
         try:
-            server = smtplib.SMTP(smtp_server, smtp_port)
+            name = request.form['name']
+            email = request.form['email']
+            subject = request.form['subject']
+            message = request.form['message']
+
+            sender_email = email_username
+            sender_password = email_password
+            recipient = recipient_email
+
+            msg = MIMEMultipart()
+            msg['From'] = sender_email
+            msg['To'] = recipient
+            msg['Subject'] = 'New Contact Form Submission'
+
+            body = f"From: {email}\nSubject: {subject}\nName: {name}\nMessage:\n{message}"
+
+            msg.attach(MIMEText(body, 'plain'))
+
+            server = smtplib.SMTP('smtp.gmail.com', 587)
             server.starttls()
-            server.login(email_username, email_password)
-            server.sendmail(email_username, 'mikemanuu273@gmail.com', msg)
+            server.login(sender_email, sender_password, )
+            server.sendmail(send_email, recipient, msg.as_string())
             server.quit()
-            return 'Email sent successfully!'
+
+            return 'Email sent successfully'
+
         except Exception as e:
-            return 'Email could not be sent. Please try again later.'
+            return f'An error occured: {str(e)}'
+    else:
+        return 'Method not allowed'
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ form-validation.js
@@ -0,0 +1,40 @@
+//document.addEventListener('DOMContentLoaded', function () {
+  //  const form = document.querySelector('form');
+  //  const name_input = document.getElementById('name');
+  //  const email_input = document.getElementById('email');
+  //  const subject_input = document.getElementById('subject')
+  //  const message_input = document.getElementById('message');
+
+  //  function validateEmail(email) {
+  //      const email_pattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+   //     return email_pattern.test(email);
+  //  }
+
+  
+    //form.addEventListener('submit', function (event) {
+     //   event.preventDefault();
+     //   if (name_input.value.trim() === '') {
+     //       alert('Name is required!');
+      //      return;
+      //  }
+
+      //  if (!validateEmail(email_input.value)) {
+      //      alert('Invalid email format');
+      //      return;
+      //  }
+
+       // if (subject_input.value.trim() === '') {
+        //    alert('Subject is required!')
+        //    return;
+      //  }
+
+      //  if (message_input.value.trim() === '') {
+       //     alert('Message is required!');
+        //    return;
+       // }
+
+        //alert('Message sent successfully!');
+
+       // form.reset();
+   // });
+//});
